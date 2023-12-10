afda3142f1e39dfac81251346ff836134e70d8cd
Risto Vatsar <r.vatsar@gmail.com>
2023-12-10 13:30:30 +0200

    first commit

---
+++ sqlDataBase.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE myrecipes;
+
+CREATE TABLE
+    recipe (id SERIAL PRIMARY KEY, recipeName VARCHAR(55));
+
+INSERT INTO
+    recipe (recipeName)
+VALUES
+    ('Pumpkin Pasties'),
+    ('Pumpkin Tartlets'),
+    ('Creamy Pumpkin Soup');
+
+CREATE TABLE
+    ingredient (id SERIAL PRIMARY KEY, ingredientName VARCHAR(55));
+
+INSERT INTO
+    ingredient (ingredientName)
+VALUES
+    ('pumpkin puree'),
+    ('sugar'),
+    ('cinnamon'),
+    ('nutmeg'),
+    ('cloves'),
+    ('Pastry dough'),
+    ('Egg wash (1 egg beaten with a splash of milk)'),
+    ('brown sugar'),
+    ('ginger'),
+    ('Mini tart shells'),
+    ('Whipped cream for garnish'),
+    ('onion, chopped'),
+    ('garlic, minced'),
+    ('vegetable broth'),
+    ('heavy cream'),
+    ('Salt and pepper to taste');
+
+CREATE TABLE
+    ingredientInRecipe (
+        id SERIAL PRIMARY KEY,
+        recipeId INTEGER,
+        ingredientId INTEGER,
+        FOREIGN KEY (recipeId) REFERENCES recipe (id),
+        FOREIGN KEY (ingredientId) REFERENCES ingredient (id)
+    );
+
+SELECT
+    *
+FROM
+    recipe;
+
+SELECT
+    *
+FROM
+    ingredient;
