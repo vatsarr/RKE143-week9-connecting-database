6334370e1f739ee97d746ea006b7d992cd2c38ea
Risto Vatsar <r.vatsar@gmail.com>
2023-12-10 18:19:20 +0200

    smol fix

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 const itemsPool = new Pool({
     user: "postgres",
-    password: "Paroolikene",
+    password: "admin1234",
     host: "localhost",
     port: 5432,
     database: "myrecipes",
